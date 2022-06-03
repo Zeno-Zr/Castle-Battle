9bbb996213f9b8d171e555458311ff76d4b77dd7
Toh-HongFeng <hf.toh2@gmail.com>
2022-06-03 15:40:11 +0800

    added movement AI for melee and ranged mobs

---
+++ Unity/new/Assets/Scripts/RangedMobAI.cs
@@ -0,0 +1,109 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Pathfinding;
+
+namespace RangedMobAI
+{
+public class MobAI : MonoBehaviour
+{
+    public Transform target;
+    public LayerMask targetLayer;
+    public float speed = 200f;
+    public float aggroRange = 100f;
+    public float attackRange = 50f;
+    public float nextWaypointDistance = 3f;
+
+    public Transform enemyGFX;
+
+    Path path;
+    int currentWaypoint = 0;
+    //bool reachedEndOfPath = false;
+
+    Seeker seeker;
+    Rigidbody2D rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        seeker = GetComponent<Seeker>();
+        rb = GetComponent<Rigidbody2D>();
+
+        InvokeRepeating("UpdatePath", 0f, 0.5f);
+    }
+
+    void UpdatePath()
+    {
+        if (!isInPatrolRange() || isInAttackRange())
+        {
+            path = null;
+        }
+        else if (seeker.IsDone())
+        {
+            seeker.StartPath(rb.position, target.position, OnPathComplete);
+        }
+    }
+
+    void OnPathComplete(Path p)
+    {
+        if (!p.error)
+        {
+            path = p;
+            currentWaypoint = 0;
+        }
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (path == null)
+        {
+            return;
+        }
+
+        if (currentWaypoint >= path.vectorPath.Count)
+        {
+            //reachedEndOfPath = true;
+            return;
+        } 
+        /*
+        else
+        {
+            reachedEndOfPath = false;
+        }
+        */
+
+        Vector2 direction = ((Vector2)path.vectorPath[currentWaypoint] - rb.position).normalized;
+        Vector2 force = direction * speed * Time.deltaTime;
+
+        rb.AddForce(force);
+
+        float distance = Vector2.Distance(rb.position, path.vectorPath[currentWaypoint]);
+
+        if (distance < nextWaypointDistance)
+        {
+            currentWaypoint++;
+        }
+
+        if (force.x >= 0.01f)
+        {
+            enemyGFX.localScale = new Vector3(-1f, 1f, 1f);
+        }
+        else if (rb.velocity.x <= -0.01f)
+        {
+            enemyGFX.localScale = new Vector3(1f, 1f, 1f);
+        }
+    }
+
+    // Checks if target is in aggro range
+    bool isInPatrolRange()
+    {
+        return Physics2D.OverlapCircle(rb.position, aggroRange, targetLayer);
+    }
+
+    bool isInAttackRange()
+    {
+        return Physics2D.OverlapCircle(rb.position, attackRange, targetLayer);
+    }
+}
+}
