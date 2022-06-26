2576aa0fb64ee050c6ed5ae2875d5c4549eac495
Toh-HongFeng <hf.toh2@gmail.com>
2022-06-26 22:26:17 +0800

    improved efficiency of floating platform script

--- Unity/new/Assets/Scripts/FloatingPlatformPath.cs
+++ Unity/new/Assets/Scripts/FloatingPlatformPath.cs
@@ -14,7 +14,7 @@ public class FloatingPlatformPath : MonoBehaviour
     Rigidbody2D rb;
     float TimeX;
     float TimeY;
-    bool IsDone = false;
+    //bool IsDone = false;
 
     // Start is called before the first frame update
     void Start()
@@ -29,14 +29,14 @@ public class FloatingPlatformPath : MonoBehaviour
         }
     }
 
-    void Update()
-    {
-        if (speed != 0 && IsDone == true)
-        {
-            IsDone = false;
-            StartCoroutine(Wander(pathNumber));
-        }
-    }
+    // void Update()
+    // {
+    //     if (speed != 0 && IsDone == true)
+    //     {
+    //         IsDone = false;
+    //         StartCoroutine(Wander(pathNumber));
+    //     }
+    // }
 
     IEnumerator DelayBeforeStart()
     {
@@ -49,148 +49,190 @@ public class FloatingPlatformPath : MonoBehaviour
         // Moves clockwise, starts with x++
         if (WanderPath == 0)
         {
-            IncreaseX();
-            yield return new WaitForSeconds(TimeX);
-            DecreaseY();
-            yield return new WaitForSeconds(TimeY);
-            DecreaseX();
-            yield return new WaitForSeconds(TimeX);
-            IncreaseY();
-            yield return new WaitForSeconds(TimeY);
+            while (true)
+            {
+                IncreaseX();
+                yield return new WaitForSeconds(TimeX);
+                DecreaseY();
+                yield return new WaitForSeconds(TimeY);
+                DecreaseX();
+                yield return new WaitForSeconds(TimeX);
+                IncreaseY();
+                yield return new WaitForSeconds(TimeY);
+            }
         }
         // Moves Anticlockwise, starts with y--
         else if (WanderPath == 13)
         {
-            DecreaseY();
-            yield return new WaitForSeconds(TimeY);
-            IncreaseX();
-            yield return new WaitForSeconds(TimeX);
-            IncreaseY();
-            yield return new WaitForSeconds(TimeY);
-            DecreaseX();
-            yield return new WaitForSeconds(TimeX);
+            while (true)
+            {
+                DecreaseY();
+                yield return new WaitForSeconds(TimeY);
+                IncreaseX();
+                yield return new WaitForSeconds(TimeX);
+                IncreaseY();
+                yield return new WaitForSeconds(TimeY);
+                DecreaseX();
+                yield return new WaitForSeconds(TimeX);
+            }
         }
 
         // Taking reference to a clock face, moves from centre to the input number
         // Moves one axis at a time
         else if (WanderPath == 1)
         {
-            IncreaseY();
-            yield return new WaitForSeconds(TimeY);
-            IncreaseX();
-            yield return new WaitForSeconds(TimeX);
-            DecreaseX();
-            yield return new WaitForSeconds(TimeX);
-            DecreaseY();
-            yield return new WaitForSeconds(TimeY);
+            while (true)
+            {
+                IncreaseY();
+                yield return new WaitForSeconds(TimeY);
+                IncreaseX();
+                yield return new WaitForSeconds(TimeX);
+                DecreaseX();
+                yield return new WaitForSeconds(TimeX);
+                DecreaseY();
+                yield return new WaitForSeconds(TimeY);
+            }
         }
         else if (WanderPath == 2)
         {
-            IncreaseX();
-            yield return new WaitForSeconds(TimeX);
-            IncreaseY();
-            yield return new WaitForSeconds(TimeY);
-            DecreaseY();
-            yield return new WaitForSeconds(TimeY);
-            DecreaseX();
-            yield return new WaitForSeconds(TimeX);
+            while (true)
+            {
+                IncreaseX();
+                yield return new WaitForSeconds(TimeX);
+                IncreaseY();
+                yield return new WaitForSeconds(TimeY);
+                DecreaseY();
+                yield return new WaitForSeconds(TimeY);
+                DecreaseX();
+                yield return new WaitForSeconds(TimeX);
+            }
         }
         else if (WanderPath == 3)
         {
-            IncreaseX();
-            yield return new WaitForSeconds(TimeX);
-            DecreaseX();
-            yield return new WaitForSeconds(TimeX);
+            while (true)
+            {
+                IncreaseX();
+                yield return new WaitForSeconds(TimeX);
+                DecreaseX();
+                yield return new WaitForSeconds(TimeX);
+            }
         }
         else if (WanderPath == 4)
         {
-            IncreaseX();
-            yield return new WaitForSeconds(TimeX);
-            DecreaseY();
-            yield return new WaitForSeconds(TimeY);
-            IncreaseY();
-            yield return new WaitForSeconds(TimeY);
-            DecreaseX();
-            yield return new WaitForSeconds(TimeX);
+            while (true)
+            {
+                IncreaseX();
+                yield return new WaitForSeconds(TimeX);
+                DecreaseY();
+                yield return new WaitForSeconds(TimeY);
+                IncreaseY();
+                yield return new WaitForSeconds(TimeY);
+                DecreaseX();
+                yield return new WaitForSeconds(TimeX);
+            }
         }
         if (WanderPath == 5)
         {
-            DecreaseY();
-            yield return new WaitForSeconds(TimeY);
-            IncreaseX();
-            yield return new WaitForSeconds(TimeX);
-            DecreaseX();
-            yield return new WaitForSeconds(TimeX);
-            IncreaseY();
-            yield return new WaitForSeconds(TimeY);
+            while (true)
+            {
+                DecreaseY();
+                yield return new WaitForSeconds(TimeY);
+                IncreaseX();
+                yield return new WaitForSeconds(TimeX);
+                DecreaseX();
+                yield return new WaitForSeconds(TimeX);
+                IncreaseY();
+                yield return new WaitForSeconds(TimeY);
+            }
         }
         if (WanderPath == 6)
         {
-            DecreaseY();
-            yield return new WaitForSeconds(TimeY);
-            IncreaseY();
-            yield return new WaitForSeconds(TimeY);
+            while (true)
+            {
+                DecreaseY();
+                yield return new WaitForSeconds(TimeY);
+                IncreaseY();
+                yield return new WaitForSeconds(TimeY);
+            }
         }
         if (WanderPath == 7)
         {
-            DecreaseY();
-            yield return new WaitForSeconds(TimeY);
-            DecreaseX();
-            yield return new WaitForSeconds(TimeX);
-            IncreaseX();
-            yield return new WaitForSeconds(TimeX);
-            IncreaseY();
-            yield return new WaitForSeconds(TimeY);
+            while (true)
+            {
+                DecreaseY();
+                yield return new WaitForSeconds(TimeY);
+                DecreaseX();
+                yield return new WaitForSeconds(TimeX);
+                IncreaseX();
+                yield return new WaitForSeconds(TimeX);
+                IncreaseY();
+                yield return new WaitForSeconds(TimeY);
+            }
         }
         if (WanderPath == 8)
         {
-            DecreaseX();
-            yield return new WaitForSeconds(TimeX);
-            DecreaseY();
-            yield return new WaitForSeconds(TimeY);
-            IncreaseY();
-            yield return new WaitForSeconds(TimeY);
-            IncreaseX();
-            yield return new WaitForSeconds(TimeX);
+            while (true)
+            {
+                DecreaseX();
+                yield return new WaitForSeconds(TimeX);
+                DecreaseY();
+                yield return new WaitForSeconds(TimeY);
+                IncreaseY();
+                yield return new WaitForSeconds(TimeY);
+                IncreaseX();
+                yield return new WaitForSeconds(TimeX);
+            }
         }
         if (WanderPath == 9)
         {
-            DecreaseX();
-            yield return new WaitForSeconds(TimeX);
-            IncreaseX();
-            yield return new WaitForSeconds(TimeX);
+            while (true)
+            {
+                DecreaseX();
+                yield return new WaitForSeconds(TimeX);
+                IncreaseX();
+                yield return new WaitForSeconds(TimeX);
+            }
         }
         if (WanderPath == 10)
         {
-            DecreaseX();
-            yield return new WaitForSeconds(TimeX);
-            IncreaseY();
-            yield return new WaitForSeconds(TimeY);
-            DecreaseY();
-            yield return new WaitForSeconds(TimeY);
-            IncreaseX();
-            yield return new WaitForSeconds(TimeX);
+            while (true)
+            {
+                DecreaseX();
+                yield return new WaitForSeconds(TimeX);
+                IncreaseY();
+                yield return new WaitForSeconds(TimeY);
+                DecreaseY();
+                yield return new WaitForSeconds(TimeY);
+                IncreaseX();
+                yield return new WaitForSeconds(TimeX);
+            }
         }
         if (WanderPath == 11)
         {
-            IncreaseY();
-            yield return new WaitForSeconds(TimeY);
-            DecreaseX();
-            yield return new WaitForSeconds(TimeX);
-            IncreaseX();
-            yield return new WaitForSeconds(TimeX);
-            DecreaseY();
-            yield return new WaitForSeconds(TimeY);
+            while (true)
+            {
+                IncreaseY();
+                yield return new WaitForSeconds(TimeY);
+                DecreaseX();
+                yield return new WaitForSeconds(TimeX);
+                IncreaseX();
+                yield return new WaitForSeconds(TimeX);
+                DecreaseY();
+                yield return new WaitForSeconds(TimeY);
+            }
         }
         if (WanderPath == 12)
         {
-            IncreaseY();
-            yield return new WaitForSeconds(TimeY);
-            DecreaseY();
-            yield return new WaitForSeconds(TimeY);
+            while (true)
+            {
+                IncreaseY();
+                yield return new WaitForSeconds(TimeY);
+                DecreaseY();
+                yield return new WaitForSeconds(TimeY);
+            }
         }
 
-        IsDone = true;
+        //IsDone = true;
     }
 
     void IncreaseX()
