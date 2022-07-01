1e95446a9bf1f3416968d43ca770cf133fd1da11
Toh-HongFeng <hf.toh2@gmail.com>
2022-07-01 21:02:59 +0800

    added aiming laser for TankBoss enraged mode

--- Unity/new/Assets/Castle-Battle Assets/Objects/enemy/Tank_Boss/TankBossAttack.cs
+++ Unity/new/Assets/Castle-Battle Assets/Objects/enemy/Tank_Boss/TankBossAttack.cs
@@ -6,14 +6,20 @@ public class TankBossAttack : MonoBehaviour
 {
     public GameObject tankBossBullet;
     public Transform attackPos;
-    public int bulletSalvoCount = 3;
+    public LineRenderer lineRenderer;
+
+    [SerializeField] private int bulletSalvoCount = 3;
+    [SerializeField] private float laserMaxRange = 20f;
+    [SerializeField] private float laserChargeTime = 3f;
     
     float intervalBetweenSalvo = 0.3f;
+    Transform player;
 
     // Start is called before the first frame update
     void Start()
     {
         attackPos = transform.Find("attackPos");
+        player = GameObject.Find("Player_armor").transform;
     }
 
     public void Attack()
@@ -33,6 +39,34 @@ public class TankBossAttack : MonoBehaviour
 
     public void EnragedAttack()
     {
-        //shoot laser
+        //raycast draw aiming laser
+        Vector2 target = new Vector2(player.position.x, player.position.y);
+        if (Physics2D.Raycast(attackPos.position, target))
+        {
+            RaycastHit2D _hit = Physics2D.Raycast(attackPos.position, target);
+            Draw2DRay(attackPos.position, _hit.point);
+        }
+        else
+        {
+            Draw2DRay(attackPos.position, target.normalized * laserMaxRange);
+        }
+        //instantiate damage laser
+        StartCoroutine(DamageLaser());
+    }
+
+    void Draw2DRay(Vector2 startPos, Vector2 endPos)
+    {
+        lineRenderer.SetPosition(0, startPos);
+        lineRenderer.SetPosition(1, endPos);
+    }
+
+    private IEnumerator DamageLaser()
+    {
+        yield return new WaitForSeconds(laserChargeTime);
+        //adjust linerenderer width
+        //set mesh collider
+        //set tag damage
+        //set linerenderer array null
+        lineRenderer.SetPosition(1, attackPos.position);
     }
 }
