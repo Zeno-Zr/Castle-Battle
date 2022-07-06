e0dad34dedb08148b8990b248d6b9e0be6d72e02
Toh-HongFeng <hf.toh2@gmail.com>
2022-07-06 20:16:44 +0800

    added StateMachine for final story boss

--- Unity/new/Assets/Scripts/GateOpenClose.cs
+++ Unity/new/Assets/Scripts/GateOpenClose.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class GateOpenClose : MonoBehaviour
 {
+    public EnemyStats enemyStats;
+
     Rigidbody2D rb;
     bool GateClosed = false;
     // Start is called before the first frame update
@@ -12,21 +14,21 @@ public class GateOpenClose : MonoBehaviour
         rb = gameObject.GetComponent<Rigidbody2D>();
     }
 
+    void Update()
+    {
+        if (GateClosed && enemyStats.enemyHealthAmount <= 0)
+        {
+            rb.MovePosition(rb.position + Vector2.up * 2.5f);
+            Destroy(this);
+        };
+    }
+
     void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.tag == "Player" && !GateClosed)
         {
             rb.MovePosition(rb.position + Vector2.down * 2.5f);
             GateClosed = true;
-        }
-    }
-
-    public void OpenGate()
-    {
-        if (GateClosed)
-        {
-            rb.MovePosition(rb.position + Vector2.up * 2.5f);
-            Destroy(this);
         }
     }
 }
